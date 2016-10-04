1a82126301fa1fbc4d4a029d5dd9bb989199921b
Fernando Meireles <fernando.meireles@ufrgs.br>
2016-10-04 14:25:51 -0300

    Update theme_blog

--- R/theme_blog.R
+++ R/theme_blog.R
@@ -3,22 +3,29 @@
 #' A ggplot2 theme for my blog plots.
 #'
 #'
-#' @param tam.fonte Font size
+#' @param tamanho Font size
 #' @param fonte Font type
+#' @param title.fonte Title's font type
 #'
 #' @import ggplot2
 #'
 #' @export
 
-theme_blog <- function (tam.fonte = 14, fonte = "sans") {
+theme_blog <- function(tamanho = 14, fonte = "sans", title.fonte = NULL){
 
-  (ggplot2::theme_minimal(base_size = tam.fonte, base_family = fonte) +
+  if(is.null(title.fonte)) title.fonte <- fonte
+
+  (ggplot2::theme_minimal(base_size = tamanho, base_family = fonte) +
      ggplot2::theme(panel.grid.minor = element_blank(),
                     panel.grid.major.x = element_blank(),
                     panel.grid.major.y = element_line(colour = "grey80", size = 0.25),
-                    axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 20, 0, 0)),
-                    axis.title.x = ggplot2::element_text(margin = ggplot2::margin(20, 0, 0, 0)),
-                    plot.caption = ggplot2::element_text(margin = ggplot2::margin(10, 0, 0, 0)),
-                    plot.title = ggplot2::element_text(face = "bold", size = ggplot2::rel(1.5)),
-                    plot.subtitle = ggplot2::element_text(size = ggplot2::rel(1.1))))
+                    axis.text = element_text(size = ggplot2::rel(0.75)),
+                    axis.title.y = ggplot2::element_text(size = ggplot2::rel(0.9), margin = ggplot2::margin(0, 20, 0, 0)),
+                    axis.title.x = ggplot2::element_text(size = ggplot2::rel(0.9), margin = ggplot2::margin(20, 0, 0, 0)),
+                    plot.caption = ggplot2::element_text(size = ggplot2::rel(0.6), margin = ggplot2::margin(10, 0, 0, 0)),
+                    plot.title = ggplot2::element_text(face = "bold", size = ggplot2::rel(1.4), margin = ggplot2::margin(0, 0, 13, 0), family = title.fonte),
+                    plot.subtitle = ggplot2::element_text(size = ggplot2::rel(0.85), margin = ggplot2::margin(0, 0, 15, 0)),
+                    legend.position = "bottom",
+                    legend.title = element_blank(),
+                    legend.margin = ggplot2::margin(12, 0, 0, 0)))
 }
