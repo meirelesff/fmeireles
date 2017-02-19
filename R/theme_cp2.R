d68531db01bf39e43197d97dff5660fb1c1577f1
Fernando Meireles <fernando.meireles@ufrgs.br>
2017-02-19 14:56:44 -0300

    Updata theme_cp2

--- R/theme_cp2.R
+++ R/theme_cp2.R
@@ -16,6 +16,8 @@ theme_cp2 <- function (tam.fonte = 12, fonte = "sans") {
      ggplot2::theme(plot.title = ggplot2::element_text(vjust = 0, margin = ggplot2::margin(0, 0, 20, 0)),
                     axis.title.y = ggplot2::element_text(angle = 90, margin = ggplot2::margin(0, 15)),
                     axis.title.x = ggplot2::element_text(margin = ggplot2::margin(15)),
+                    axis.title = ggplot2::element_text(size = tam.fonte),
+                    title = ggplot2::element_text(size = tam.fonte),
                     legend.position = "bottom",
                     legend.title = ggplot2::element_blank(),
                     legend.key = ggplot2::element_blank(),
